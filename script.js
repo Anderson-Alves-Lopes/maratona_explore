63da3f471d8260f770f3400b8f8977392947c777
Anderson Alves <nenoalveslopes@hotmail.com>
2022-07-09 12:30:47 -0300

    Projeto maratona Explore

---
+++ script.js
@@ -0,0 +1,17 @@
+function handleDark(stop){
+    document.querySelector('#body').classList.remove('dark');
+    document.querySelector('#body').classList.add('light');
+    document.querySelector('#black').style.display = 'none';
+    document.querySelector('#white').style.display = 'block';
+    document.querySelector('.container p').style.color = '#000000';
+    document.querySelector('.section-button').style.color = '#000000';
+}
+function handleLight(stop){
+    document.querySelector('#body').classList.remove('light');
+    document.querySelector('#body').classList.add('dark');
+    document.querySelector('#black').style.display = 'block';
+    document.querySelector('#white').style.display = 'none';
+    document.querySelector('.container p').style.color = '#ffffff';
+    document.querySelector('.section-button').style.color = '#ffffff';
+    
+}
