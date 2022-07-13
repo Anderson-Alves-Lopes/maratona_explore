157365fd4000c8d4785860e6cae759c3596a43c0
Anderson Alves <nenoalveslopes@hotmail.com>
2022-07-13 08:33:22 -0300

    Modificações no javascript para deixar o transition mais suave

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function handleDark(stop){
     document.querySelector('#body').classList.remove('dark');
     document.querySelector('#body').classList.add('light');
+    document.querySelector('#body').style.transition = 'all 1s';
     document.querySelector('#black').style.display = 'none';
     document.querySelector('#white').style.display = 'block';
     document.querySelector('.container p').style.color = '#000000';
@@ -9,6 +10,7 @@ function handleDark(stop){
 function handleLight(stop){
     document.querySelector('#body').classList.remove('light');
     document.querySelector('#body').classList.add('dark');
+    document.querySelector('#body').style.transition = 'all 1s';
     document.querySelector('#black').style.display = 'block';
     document.querySelector('#white').style.display = 'none';
     document.querySelector('.container p').style.color = '#ffffff';
